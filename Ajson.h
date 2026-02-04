c78a87ae2600e2ceb49dfb85296c1c442f124863
Sergey_Balbashov <142932792+Sergeras1@users.noreply.github.com>
2026-02-04 13:51:29 +0500

    Add files via upload

--- Ajson.h
+++ Ajson.h
@@ -7,10 +7,10 @@ typedef struct {
 }JsonTypedef;
 
 JsonTypedef* json_init();
-void json_append(JsonTypedef* json, char* str);
+void json_append(JsonTypedef* json, const char* str);
 void json_begin_object(JsonTypedef* json);
 void json_end_object(JsonTypedef* json);
-void json_save_file(JsonTypedef* json, char* filename);
+void json_save_file(JsonTypedef* json, const char* filename);
 void json_free(JsonTypedef* json);
 void json_write_string(JsonTypedef* json, const char* key, const char* value);
 void json_write_int(JsonTypedef* json, const char* key, int value);
